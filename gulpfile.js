b9b9684236ca72c51f6757860822caf29158ea45
Egenie-mih <astra2826@gmail.com>
2017-04-27 19:20:50 +0300

    Исправляет путь до шрифта

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task("clean", function() {
 
 gulp.task("copy", function() {
   return gulp.src([
-    "fonts/**/*.{woff, woff2}",
+    "fonts/*.{woff,woff2}",
     "img/**",
     "js/**",
     "*.html"
